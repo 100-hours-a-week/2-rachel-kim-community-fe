d5b6c796f9d484978658ad8f31fa4fff3500eb05
Myungbeem Kim <128389329+mbeankong@users.noreply.github.com>
2024-11-20 17:22:37 +0900

    경로 수정

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ app.use('/assets', express.static(path.join(__dirname, 'assets')));
 app.use('/css', express.static(path.join(__dirname, 'css')));
 
 // 각 HTML 파일을 요청에 맞게 응답하는 경로 설정
-app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, 'html', 'login.html'));  // 로그인 페이지
+app.get('/log-in', (req, res) => {
+  res.sendFile(path.join(__dirname, 'html', 'log-in.html'));  // 로그인 페이지
 });
 
-app.get('/sign-up', (req, res) => {
-  res.sendFile(path.join(__dirname, 'html', 'sign-up.html'));  // 회원가입 페이지
+app.get('/sign-in', (req, res) => {
+  res.sendFile(path.join(__dirname, 'html', 'sign-in.html'));  // 회원가입 페이지
 });
 
 app.get('/edit-profile', (req, res) => {
@@ -25,25 +25,25 @@ app.get('/edit-password', (req, res) => {
   res.sendFile(path.join(__dirname, 'html', 'edit-password.html'));  // 비밀번호 변경 페이지
 });
 
-app.get('/post-create', (req, res) => {
-  res.sendFile(path.join(__dirname, 'html', 'post-create.html'));  // 게시글 작성 페이지
+app.get('/make-post', (req, res) => {
+  res.sendFile(path.join(__dirname, 'html', 'make-post.html'));  // 게시글 작성 페이지
 });
 
-app.get('/post-detail', (req, res) => {
-  res.sendFile(path.join(__dirname, 'html', 'post-detail.html'));  // 게시글 상세 페이지
+app.get('/post', (req, res) => {
+  res.sendFile(path.join(__dirname, 'html', 'post.html'));  // 게시글 상세 페이지
 });
 
-app.get('/post-edit', (req, res) => {
-  res.sendFile(path.join(__dirname, 'html', 'post-edit.html'));  // 게시글 수정 페이지
+app.get('/edit-post', (req, res) => {
+  res.sendFile(path.join(__dirname, 'html', 'edit-post.html'));  // 게시글 수정 페이지
 });
 
-app.get('/post-list', (req, res) => {
-  res.sendFile(path.join(__dirname, 'html', 'post-list.html'));  // 게시글 목록 페이지
+app.get('/posts', (req, res) => {
+  res.sendFile(path.join(__dirname, 'html', 'posts.html'));  // 게시글 목록 페이지
 });
 
 // 기본 페이지 (로그인 페이지로 리다이렉트)
 app.get('/', (req, res) => {
-  res.redirect('/login');  // 기본적으로 로그인 페이지로 리다이렉트
+  res.redirect('/log-in');  // 기본적으로 로그인 페이지로 리다이렉트
 });
 
 // 서버 시작
