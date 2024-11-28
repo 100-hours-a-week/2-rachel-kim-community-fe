8decb6b7a5b31b1fa976946487ead16cb186b526
Myungbeem Kim <128389329+mbeankong@users.noreply.github.com>
2024-11-28 14:59:48 +0900

    js로 인한 수정

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// 정적 파일 제공 (CSS, 이미지, 폰트 등)
+// 정적 파일 제공 (CSS, 이미지, 폰트, JS 등)
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/js', express.static(path.join(__dirname, 'js')));
 
 // 각 HTML 파일을 요청에 맞게 응답하는 경로 설정
 app.get('/log-in', (req, res) => {
@@ -17,28 +18,28 @@ app.get('/sign-in', (req, res) => {
   res.sendFile(path.join(__dirname, 'html', 'sign-in.html'));  // 회원가입 페이지
 });
 
+app.get('/posts', (req, res) => {
+  res.sendFile(path.join(__dirname, 'html', 'posts.html'));  // 게시글 목록 페이지
+});
+
+app.get('/post/:postId', (req, res) => {
+  res.sendFile(path.join(__dirname, 'html', 'post.html'));  // 게시글 상세 페이지
+});
+
+app.get('/post/create', (req, res) => {
+  res.sendFile(path.join(__dirname, 'html', 'make-post.html'));  // 게시글 작성 페이지
+});
+
+app.get('/post/edit/:postId', (req, res) => {
+  res.sendFile(path.join(__dirname, 'html', 'edit-post.html'));  // 게시글 수정 페이지
+});
+
 app.get('/edit-profile', (req, res) => {
   res.sendFile(path.join(__dirname, 'html', 'edit-profile.html'));  // 프로필 수정 페이지
 });
 
 app.get('/edit-password', (req, res) => {
   res.sendFile(path.join(__dirname, 'html', 'edit-password.html'));  // 비밀번호 변경 페이지
-});
-
-app.get('/make-post', (req, res) => {
-  res.sendFile(path.join(__dirname, 'html', 'make-post.html'));  // 게시글 작성 페이지
-});
-
-app.get('/post', (req, res) => {
-  res.sendFile(path.join(__dirname, 'html', 'post.html'));  // 게시글 상세 페이지
-});
-
-app.get('/edit-post', (req, res) => {
-  res.sendFile(path.join(__dirname, 'html', 'edit-post.html'));  // 게시글 수정 페이지
-});
-
-app.get('/posts', (req, res) => {
-  res.sendFile(path.join(__dirname, 'html', 'posts.html'));  // 게시글 목록 페이지
 });
 
 // 기본 페이지 (로그인 페이지로 리다이렉트)
